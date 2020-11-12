5069abc0a573e6fbdba0cf11bcbee149e4794625
Konstantin Ivanov <69236482+TredNR@users.noreply.github.com>
2020-11-12 18:49:06 +0400

    Create lxml.py

---
+++ lxml.py
@@ -0,0 +1,9 @@
+import requests
+from lxml import html
+
+response = requests.get('http://ya.ru')
+
+parsed_body = html.fromstring(response.text)
+
+print(parsed_body.xpath('//title/text()')[0]) 
+print(parsed_body.xpath('//a/@href'))         
