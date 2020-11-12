deb0892075155308789bba190bd998df4801e263
Konstantin Ivanov <69236482+TredNR@users.noreply.github.com>
2020-11-12 19:05:29 +0400

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+wd = webdriver.Chrome('chromedriver',options=options)
+wd.get("https://www.website.com")
+print(wd.page_source) 
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
